a2ac10548455901dddb7c004e96f4f226abf1b7e
elpbatista <elpbatista@gmail.com>
2021-11-10 03:47:45 -0800

    just a nicer better query

--- queries.sql
+++ queries.sql
@@ -212,3 +212,57 @@ FROM teste_pts_medellin pts
 	) vias;
 -- WHERE vias.nombre_com IS NOT NULL
 -- LIMIT 3000;
+-- ************************************************************************
+-- 
+-- ************************************************************************
+WITH administrative AS (
+	SELECT *
+	FROM jplanet_osm_polygon
+	WHERE tags->>'boundary' = 'administrative'
+		AND tags->>'admin_level' = '6'
+)
+SELECT pts.feature_id AS _id,
+	pts.geom AS geom,
+	pts.properties->>'via' AS via,
+	pts.properties->>'house_number' AS placa,
+	pts.properties->>'tipo_cruce' AS cruce,
+	nvias.via_name AS nombre,
+	nvias.nombre_com AS nombrecom,
+	administrative.tags->>'divipola' AS divipola,
+	pts.properties->>'nombre_bar' AS barrio,
+	pts.properties->>'nombre_com' AS comunna,
+	SUBSTRING(administrative.tags->>'name', '^[^,]+') AS city,
+	administrative.tags->>'is_in:state' AS munipality,
+	administrative.tags->>'is_in:country' AS country
+FROM teste_pts_medellin pts
+	LEFT JOIN administrative ON ST_Contains(administrative.way, pts.geom)
+	CROSS JOIN LATERAL (
+		SELECT vias.properties->>'label' AS via_label,
+			CASE
+				WHEN vias.properties->>'via_name' = pts.properties->>'via' THEN ''
+         		WHEN pts.properties->>'via' != vias.properties->>'label' THEN ''
+         		ELSE vias.properties->>'via_name' END
+         		AS via_name,
+			CASE
+				WHEN vias.properties->>'via_name' = vias.properties->>'nombre_com' THEN ''
+         		ELSE vias.properties->>'nombre_com' END
+         		AS nombre_com,
+			vias.geom<->pts.geom AS dist
+		FROM test_feature_asis_vias vias
+		ORDER BY dist
+		LIMIT 1
+	) nvias
+LIMIT 1000;
+-- +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+-- 
+-- +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+SELECT 
+  a.id, a.name, 
+  ( 
+    SELECT json_agg(item)
+    FROM (
+      SELECT b.c1 AS x, b.c2 AS y 
+      FROM b WHERE b.item_id = a.id
+    ) item
+  ) AS items
+FROM a;

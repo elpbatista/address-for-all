ae0e7225e8a663ba5186ad35c47a28fdc9ecacfc
elpbatista <elpbatista@gmail.com>
2021-12-10 01:43:11 -0800

    placing the zoom control

--- web/main.js
+++ web/main.js
@@ -1,11 +1,14 @@
 window.onload = () => {
-  var map = L.map("map").fitWorld();
+  var map = L.map("map", {
+    zoomControl: false,
+  }).fitWorld();
 
   var tiles = L.tileLayer(
     "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw",
     {
       maxZoom: 18,
-      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+      attribution:
+        'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
         'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
       id: "mapbox/streets-v11",
       tileSize: 512,

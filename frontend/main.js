818369555ab96ea7c3aa6d187461dce6f22fd788
elpbatista <elpbatista@gmail.com>
2021-12-11 20:43:10 -0800

    importing jQuery

--- frontend/main.js
+++ frontend/main.js
@@ -1,17 +1,28 @@
 import './style.css';
-// import "ol/ol.css";
+import "ol/ol.css";
 import Map from "ol/Map";
 import View from "ol/View";
 import TileLayer from "ol/layer/Tile";
 import XYZ from "ol/source/XYZ";
+import { Attribution, defaults as defaultControls } from "ol/control";
 import { useGeographic } from "ol/proj";
+
+import $ from "jquery";
+window.jQuery = window.$ = $;
+// import "jquery-ui";
+// // $(".search").hide();
+// import ui from "jquery-ui";
 
 useGeographic();
 const centerMap = [-75.573553, 6.2443382];
 const key =
   "pk.eyJ1IjoiZWxwYmF0aXN0YSIsImEiOiJja3gyZHl5OXYxbm5yMnFxOTFtZWhqbWlhIn0.bbHJjnHrt_d9iqu4hBZgyw";
+const attribution = new Attribution({
+  collapsible: false,
+});
 
 const map = new Map({
+  controls: defaultControls({ attribution: false }).extend([attribution]),
   target: "map",
   layers: [
     new TileLayer({

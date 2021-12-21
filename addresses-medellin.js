813ae5df8dfbde70fe00263cd93637365ac4de72
elpbatista <elpbatista@gmail.com>
2021-12-21 01:19:08 -0800

    medellin export table

--- addresses-medellin.js
+++ addresses-medellin.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const _d = require("./init-data.js");
+const converter = require("json-2-csv");
 
 const nd = JSON.parse(fs.readFileSync(_d.NDPostalcode));
 const viasMedellin = JSON.parse(fs.readFileSync(_d.ViasMedellin));
@@ -10,29 +11,39 @@ const viasMedellin = JSON.parse(fs.readFileSync(_d.ViasMedellin));
 
 const ndTable = [];
 console.log(nd.features.length);
-// for (var i = 0; i < nd.features.length; i++) {
-for (var i = 0; i < 10; i++) {
+for (var i = 0; i < nd.features.length; i++) {
+// for (var i = 0; i < 1000; i++) {
   let feature = nd.features[i];
   let properties = feature.properties;
   ndTable.push({
     id: properties.OBJECTID,
     lon: feature.geometry.coordinates[0],
     lat: feature.geometry.coordinates[1],
+    number: properties.PLACA,
+    street: properties.VIA,
+    city: "Medellin",
+    district: null,
+    region: "ANT",
+    postcode: properties.CODIGO_POS,
+    hash: null,
+    // --------------------------------
     cbml: properties.CBML,
     tipo_via: properties.TIPO_VIA,
-    via: properties.VIA,
-    placa: properties.PLACA,
-    // --------------------------------
-    via_name: viasMedellin[properties.VIA] || null,
-    address: properties.VIA + " #" + properties.PLACA,
-    postalcode: properties.CODIGO_POS,
-    // --------------------------------
     tipo_cruce: properties.TIPO_CRUCE,
     codigo_man: properties.CODIGO_MAN,
     codigo_bar: properties.CODIGO_BAR,
     nombre_bar: properties.NOMBRE_BAR,
     codigo_com: properties.CODIGO_COM,
     nombre_com: properties.NOMBRE_COM,
+    // --------------------------------
+    via_name: viasMedellin[properties.VIA] || null,
+    address: properties.VIA + " #" + properties.PLACA,
+    display_name: '',
+    // housename: '',
+    // divipola: '',
   });
 }
-console.table(ndTable);
+// console.table(ndTable);
+converter.json2csv(JSON.parse(JSON.stringify(ndTable)), function (err, csv) {
+  fs.writeFileSync(_d.Output + _d.yyyymmdd + "_Medellin.csv", csv);
+});
